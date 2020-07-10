9b2669cef43db7bb566c034527e32631bb4205e2
onee <244980740@qq.com>
2020-07-10 18:41:48 +0800

    文章0710

--- article/serializers.py
+++ article/serializers.py
@@ -4,9 +4,9 @@ from rest_framework import serializers
 
 class ArticlePostSerializers(serializers.ModelSerializer):
     # serializers.PrimaryKeyRelatedField(many=True, queryset=UserProfile.objects.all())
+    author = serializers.CharField(source='author.username', read_only=True)
+    ategory = serializers.CharField(source='ategory.name', read_only=True)
 
     class Meta:
         model = ArticlePost
-        fields = [
-            'author', 'ategory', 'title', 'content', 'create', 'update'
-        ]
+        fields = '__all__'
