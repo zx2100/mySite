a1803c1c0384a13fca634f6895a8713cfa0b6b9a
onee <244980740@qq.com>
2020-07-22 18:46:05 +0800

    自定义返回

--- article/serializers.py
+++ article/serializers.py
@@ -3,29 +3,43 @@ from rest_framework import serializers
 import time
 
 
-class ArticlePostSerializers(serializers.ModelSerializer):
+# 获取文章
+class ArticleGetSerializers(serializers.ModelSerializer):
     # serializers.PrimaryKeyRelatedField(many=True, queryset=UserProfile.objects.all())
-    # author = serializers.CharField(source='author.username', read_only=True)
-    # category = serializers.CharField(source='category.name', read_only=True)
+    author = serializers.CharField(source='author.username', read_only=True)
+    category = serializers.CharField(source='category.name', read_only=True)
 
     # 重写序列化方法，对需要的字段进行自定义渲染（时间字段的返回需要序列化）
     def to_representation(self, instance):
         result = super().to_representation(instance)
         # 抽取时间，序列化时间
-        create = time.strptime(result['create'], "%Y-%m-%dT%H:%M:%S")
+        create = time.strptime(result['create'].split(".")[0], "%Y-%m-%dT%H:%M:%S")
         update = time.strptime(result['update'].split(".")[0], "%Y-%m-%dT%H:%M:%S")
         result['create'] = time.strftime('%Y-%m-%d %H:%M', create)
         result['update'] = time.strftime('%Y-%m-%d %H:%M', update)
         return result
 
-    def create(self, validated_data):
-        """
-        给定验证过的数据创建并返回一个新的 Snippet 实例。
-        """
-        return ArticlePost.objects.create(**validated_data)
-
     class Meta:
         model = ArticlePost
         fields = '__all__'
         # 获取深度，如果有字段外键的话，就往里面再次获取
-        depth = 1
+        # depth = 1
+
+
+# 提交文章
+class ArticlePostSerializers(serializers.ModelSerializer):
+
+    # def create(self, validated_data):
+    #     print(validated_data)
+    #     return ArticlePost(**validated_data)
+
+    # 验证数据
+    def validate(self, data):
+        # 验证文章简介是否超出100字符
+        if len(data["brief"]) > 100:
+            raise serializers.ValidationError("文章简介内容过长，应该小于100字符")
+        return data
+
+    class Meta:
+        model = ArticlePost
+        fields = '__all__'
--- article/views.py
+++ article/views.py
@@ -4,8 +4,21 @@ from django.http.response import HttpResponse
 from rest_framework import status
 # Create your views here.
 from .models import *
-from .serializers import ArticlePostSerializers
+from .serializers import ArticleGetSerializers, ArticlePostSerializers
 from rest_framework.views import APIView
+
+
+class MyResponse(Response):
+    # 返回渲染器
+    def __init__(self, code, msg, data, *arks, **kwargs):
+        self.ret_msg = {
+            "meta": {
+                "status": code,
+                "msg": msg
+            },
+            "data": data
+        }
+        super().__init__(self.ret_msg, *arks, **kwargs)
 
 
 class GetALLView(APIView):
@@ -14,14 +27,27 @@ class GetALLView(APIView):
     #获取文章
     def get(self, request):
         query_set = ArticlePost.objects.all()
-        result = ArticlePostSerializers(query_set, many=True)
+        result = ArticleGetSerializers(query_set, many=True)
         # print(result)
-        result = Response(result.data, status=status.HTTP_200_OK)
+        result = MyResponse(data=result.data, code=status.HTTP_200_OK, msg="获取成功")
         return result
 
     # 保存文章
-    def post(self, request):
-        print(request.data)
 
-        # result = Response({"xx": "xx"}, status=status.HTTP_200_OK)
-        return HttpResponse("xx11")
+
+class PostArticle(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        # print(request.data)
+
+        # 验证数据
+        serializer = ArticlePostSerializers(data=request.data)
+        if serializer.is_valid():
+            print("验证通过")
+            serializer.save()
+            print(serializer.data)
+        else:
+            print(serializer.errors)
+
+        return Response(serializer.errors)
